4a4bb6d4f2c917bdb78476585aac4779def194a9
red75prime <red75prim@gmail.com>
2024-12-08 22:01:55 +0500

    Initial commit

---
+++ msfs_sys/target/debug/deps/msfs_sys-25547ae3a8688b37.d
@@ -0,0 +1,7 @@
+D:\git\msfs2020_leds\msfs_sys\target\debug\deps\libmsfs_sys-25547ae3a8688b37.rmeta: src\lib.rs
+
+D:\git\msfs2020_leds\msfs_sys\target\debug\deps\libmsfs_sys-25547ae3a8688b37.rlib: src\lib.rs
+
+D:\git\msfs2020_leds\msfs_sys\target\debug\deps\msfs_sys-25547ae3a8688b37.d: src\lib.rs
+
+src\lib.rs:
